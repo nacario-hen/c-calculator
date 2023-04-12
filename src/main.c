e0b88190e56b09515fd5c9074dfb63eadd1389dd
beepboop-henny <nacario.henrico.main@gmail.com>
2023-04-12 19:59:17 +0800

    Create src folder and main code
    Directory modification commit - Create folder named src and
    add main.c source code inside.

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/*
+    Main source code of C - Calculator
+    Temporary file contents. Please remove this comment once
+    main.c is modified.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+}
